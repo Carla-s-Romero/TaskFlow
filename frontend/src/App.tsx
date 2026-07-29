69ec0d93dfd69f301cb074dd97a211ee2df643e6
Carla-s-Romero <carlasromero11@gmail.com>
2026-07-29 14:56:14 -0300

    refactoring: standardize folder and component names
    - The "cadastro" folder and its URL have been changed to "register".

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/login/signIn'
-import Cadastro from './pages/cadastro/signUp'
+import Register from './pages/register/signUp'
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
